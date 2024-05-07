dd7d0aee3dee936f61fd987b1e74cc9c23c0b715
dimmigg@gmail.com <dimmigg@gmail.com>
2024-05-07 17:27:35 +0300

    fix buttons

--- WishList.Domain/Extensions/InlineKeyboardButtonExtensions.cs
+++ WishList.Domain/Extensions/InlineKeyboardButtonExtensions.cs
@@ -8,25 +8,23 @@ public static class InlineKeyboardButtonExtensions
     public static List<List<InlineKeyboardButton>> AddBaseFooter(this List<List<InlineKeyboardButton>> keyboard,
         string? backCommand = null)
     {
-        if (!string.IsNullOrWhiteSpace(backCommand))
-            keyboard.Add([
-                InlineKeyboardButton.WithCallbackData(
-                    "« Назад", backCommand)
-            ]);
+        var backButton = InlineKeyboardButton.WithCallbackData("« Назад", backCommand ?? string.Empty);
+        var homeButton = InlineKeyboardButton.WithCallbackData("« Главное меню", Commands.MAIN);
         
-        keyboard.Add([
-            InlineKeyboardButton.WithCallbackData(
-                "« Главное меню", Commands.MAIN)
-        ]);
+        if (!string.IsNullOrWhiteSpace(backCommand))
+            keyboard.Add([backButton, homeButton]);
+        else
+            keyboard.Add([homeButton]);
 
         return keyboard;
     }
+
     public static List<List<InlineKeyboardButton>> AddSelfDeleteButton(this List<List<InlineKeyboardButton>> keyboard)
     {
         keyboard.Add([
             InlineKeyboardButton.WithCallbackData(
                 "❌ Отмена", Commands.SELF_DELETE_BUTTON)
-            ]);
+        ]);
 
         return keyboard;
     }
